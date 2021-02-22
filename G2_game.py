68d9dc27fc9fd528cd6955b8b87f74bfd8229cbe
E-gonito <masterdank47@gmail.com>
2021-02-22 17:20:05 +0000

    Create G2_game.py
    added first py file

---
+++ G2_game.py
@@ -0,0 +1,20 @@
+import simplegui, math, random
+from user305_o32FtUyCKk_0 import Vector
+
+# The canvas dimensions
+CANVAS_WIDTH = 1280
+CANVAS_HEIGHT = 720
+
+"""
+class Interaction:
+    def __init__(self:
+
+    def draw(self, canvas):
+
+"""
+
+# Create a frame and assign callbacks to event handlers
+frame = simplegui.create_frame("G2 game", CANVAS_WIDTH, CANVAS_HEIGHT)
+
+# Start the frame animation
+frame.start()
