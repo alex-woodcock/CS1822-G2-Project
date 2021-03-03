8e8ef3db3cff2b3ca3c9192760f65597c4cca65c
E-gonito <masterdank47@gmail.com>
2021-03-03 13:48:23 +0000

    Added zombie and improved bullet
    added zombie walking left for now and bullet has sound and sprite

--- G2_game.py
+++ G2_game.py
@@ -12,9 +12,12 @@ except ImportError:
 CANVAS_DIMS = (854, 480)
 CANVAS_WIDTH = 854
 CANVAS_HEIGHT = 480
-
 #Background sprite
 BACKDROP_SPRITE = simplegui.load_image('http://personal.rhul.ac.uk/zhac/315/backdrop.png')
+
+#Bullet sound
+bullet_sound = simplegui.load_sound('http://personal.rhul.ac.uk/zhac/315/bullet_shot.mp3')
+bullet_sound.set_volume(0.5)
 
 ##For non-spritesheet based sprites
 class Sprite:
@@ -22,7 +25,7 @@ class Sprite:
         self.IMG = IMG
         self.IMG_CENTRE = IMG_CENTRE
         self.IMG_DIMS = IMG_DIMS
-        self.img_dest_dim = (self.IMG_DIMS[0]*0.7, self.IMG_DIMS[1]*0.7)
+        self.img_dest_dim = (self.IMG_DIMS[0]*0.75, self.IMG_DIMS[1]*0.75)
         self.img_pos = [CANVAS_DIMS[0]/2, CANVAS_DIMS[1]/2]
 
     #Pos = Position in game world
@@ -37,16 +40,15 @@ class Spritesheet(Sprite):
 
 ##All Entities have: An assigned sprite, Position, Radius (idk what that does), Movement speed, Jump height
 class Entity():
-    def __init__(self, sprite, pos, radius, speed, jumpheight):
-        self.sprite = sprite
-        
+    def __init__(self, sprite, pos, radius, speed, jumpheight, frame_duration):
+        self.sprite = sprite      
         self.pos = pos
         self.radius = max(radius, 4)
-
         self.speed = speed
         self.jumpheight = jumpheight
-
         self.velocity = Vector(0,0)
+        self.frame_duration = frame_duration
+        self.is_dead = False
         
     #Drawing is handled by the sprite (so that spritesheets etc can be more easily handled)
     def draw(self, canvas):
@@ -60,6 +62,24 @@ class Entity():
 class Player(Entity):
     def shoot(self):
         bullets.append(Bullet())
+        
+class Zombie(Entity):
+    def update(self):
+        if self.is_dead == False:
+            self.pos.add(Vector(-0.1,0))
+            if clock.transition(self.frame_duration):
+                img_centre_x = self.sprite.IMG_CENTRE[0]
+                if (img_centre_x + 101.6) > 508:
+                    img_centre_x = 50.8            
+                self.sprite.IMG_CENTRE = (img_centre_x+101.6,55*3)
+"""
+#bullet hitting zombie not working
+        else:
+            for i in bullets:
+                if i.pos.x == self.pos.x:
+                    self.is_dead = True
+"""
+
 
 #Extends Entity
 class Bullet(Entity):
@@ -68,7 +88,7 @@ class Bullet(Entity):
     def __init__(self):
         self.sprite = bulletSprite
         ##Seems silly to do it like this, but direct referencing means shared position!
-        self.pos = Vector(player.pos.x, player.pos.y)
+        self.pos = Vector(player.pos.x+30, player.pos.y)
         self.radius = max(4, 4)
 
         self.velocity = Vector(10, 0)
@@ -113,18 +133,22 @@ class Keyboard:
             self.right = True
             self.any_input = True
         if key == simplegui.KEY_MAP['space']:
-            self.last_key = 'space'
             self.space = True
         if key == simplegui.KEY_MAP['f']:
             player.shoot()
+            bullet_sound.play()
+            bullet_sound.rewind()
+            bullet_sound.play()
         else:
             self.any_input = True
             
 
     def keyUp(self, key):
         if key == simplegui.KEY_MAP['a']:
+            self.last_key = 'a'
             self.left = False
         if key == simplegui.KEY_MAP['d']:
+            self.last_key = 'd'
             self.right = False            
         if key == simplegui.KEY_MAP['space']:
             self.space = False
@@ -144,21 +168,21 @@ class Interaction:
         ##need to animate walking animations
         if self.keyboard.left:
             self.player.sprite.IMG_CENTRE = ((610/12),(329/6)*3)
-            self.player.velocity.add(Vector(-1, 0))
+            self.player.velocity.add(Vector(-0.6, 0))
         if self.keyboard.right:
             self.player.sprite.IMG_CENTRE = ((610/12)*3,(329/6)*5)
-            self.player.velocity.add(Vector(1, 0))
+            self.player.velocity.add(Vector(0.6, 0))
         if self.keyboard.space:
             self.player.sprite.IMG_CENTRE = ((610/12)*7,(329/6))
-            self.player.velocity.add(Vector(0, -2))
+            self.player.velocity.add(Vector(0, -1.5))
         if self.keyboard.any_input == False and self.keyboard.last_key == 'd':
             self.player.sprite.IMG_CENTRE = ((610/12)*3,(329/6))
         if self.keyboard.any_input == False and self.keyboard.last_key == 'a':
-            self.player.sprite.IMG_CENTRE = ((610/12),(329/6))
-          
+            self.player.sprite.IMG_CENTRE = ((610/12),(329/6))          
         ##Jump mechanic needs fixing, dosent work with short presses and player falls too slow
         if self.player.pos.y+70 <= 480 and self.keyboard.space == False:
             self.player.velocity.add(Vector(0,1))
+        
             
             
             
@@ -175,6 +199,9 @@ def draw(canvas):
     clock.tick()
     player.update()
     player.draw(canvas)
+    zombie.update()
+    zombie.draw(canvas)
+    
 
     for i in bullets:
         i.update()
@@ -184,19 +211,20 @@ def draw(canvas):
 SHEET_URL = "http://personal.rhul.ac.uk/zhac/315/mc_spritesheet.png"
 ##player sheet dimensions 610 x 329
 
-playerSprite = Sprite(simplegui.load_image("http://personal.rhul.ac.uk/zhac/315/mc_spritesheet.png"), (150, 329/6), (100, 100))
-###NEED BULLET SPRITE PLS TY
-bulletSprite = Sprite(simplegui.load_image("http://personal.rhul.ac.uk/zhac/315/mc_spritesheet.png"), (150, 329/6), (100, 100))
-
+zombieSprite = Sprite(simplegui.load_image("http://personal.rhul.ac.uk/zhac/315/zombie_sheet.png"), (51, 55*3), (100, 100))
+bulletSprite = Sprite(simplegui.load_image("http://personal.rhul.ac.uk/zhac/315/bullet_sprite.png"), (50, 50), (100, 100))
 playerSprite = Sprite(simplegui.load_image("http://personal.rhul.ac.uk/zhac/315/mc_spritesheet.png"), ((610/12)*3, 329/6), (610/6, 329/3))
 
 kbd = Keyboard()
 clock = Clock()
 
-player = Player(playerSprite, Vector(100, 450), 50, 10, 20)
+player = Player(playerSprite, Vector(100, 450), 50, 10, 20, 30)
+zombie = Zombie(zombieSprite, Vector(700, 441), 50, 10, 20, 10)
 inter = Interaction(player, kbd)
 
 bullets = []
+zombies = []
+
 
 # Create a frame and assign callbacks to event handlers
 frame = simplegui.create_frame("G2 game", CANVAS_WIDTH, CANVAS_HEIGHT)
